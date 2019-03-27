3d422d456edbf44793de835ef31e24dc9536b6b2
hi-hi-ray <dutra.raysa@gmail.com>
2019-03-27 12:00:33 -0300

    add django form without models

--- sortido_app/forms.py
+++ sortido_app/forms.py
@@ -1,1 +1,6 @@
 from django import forms
+
+
+class UploadFileForm(forms.Form):
+    number_of_times = forms.IntegerField(label='Quantidade de Pessoas', required=True)
+    csv_file = forms.FileField(label='Arquivo CSV', required=True)
