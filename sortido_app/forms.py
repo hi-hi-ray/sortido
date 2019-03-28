4d03855d6918554be2d7e49aa8276090c531bbc9
Herik Tedesco <herik@MacBook-Pro-de-Herik.local>
2019-03-28 10:08:50 -0300

    add random form

--- sortido_app/forms.py
+++ sortido_app/forms.py
@@ -1,1 +1,7 @@
 from django import forms
+
+
+class RandomNumbersForm(forms.Form):
+    quantity_random = forms.IntegerField(label='Quantidade de vezes que será feito o sorteio', required=True)
+    first_number = forms.IntegerField(label='De', required=True)
+    second_number = forms.IntegerField(label='Até', required=True)
--- sortido_app/views.py
+++ sortido_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import *
+from random import randint
 
 
 def home(request):
@@ -9,7 +10,22 @@ def random_csv(request):
     return render(request, 'random_csv.html', {})
 
 def random_numbers(request):
-    return render(request, 'random_numbers.html', {})
+    if request.method == 'GET':
+        form = RandomNumbersForm()
+        return render(request, 'random_numbers.html', {'form': form})
+    elif request.method == 'POST':
+        form = RandomNumbersForm(request.POST)
+        if form.is_valid():
+            quantity_random = int(request.POST['quantity_random'])
+            first_number = int(request.POST['first_number'])
+            second_number = int(request.POST['second_number'])
+            numbers_of_results = []
+
+            for i in range(quantity_random):
+                numbers_of_results.append(randint(first_number, second_number))
+                print(numbers_of_results)
+
+            return render(request, 'random_numbers.html', {'form': form, 'random_done': True, 'form_result': numbers_of_results})
 
 def about(request):
     return render(request, 'about.html', {})
