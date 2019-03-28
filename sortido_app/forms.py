b952f91cf93e3a728d444d9bf1510717226cdb62
Herik Tedesco <herik@MacBook-Pro-de-Herik.local>
2019-03-28 12:19:59 -0300

    add verification fields validation

--- sortido_app/forms.py
+++ sortido_app/forms.py
@@ -3,5 +3,6 @@ from django import forms
 
 class RandomNumbersForm(forms.Form):
     quantity_random = forms.IntegerField(label='Quantidade de vezes que será feito o sorteio', required=True)
+    is_possible_same_number = forms.BooleanField(label='Quer que repetimos os números?', initial=False, required=False)
     first_number = forms.IntegerField(label='De', required=True)
     second_number = forms.IntegerField(label='Até', required=True)
--- sortido_app/views.py
+++ sortido_app/views.py
@@ -19,11 +19,27 @@ def random_numbers(request):
             quantity_random = int(request.POST['quantity_random'])
             first_number = int(request.POST['first_number'])
             second_number = int(request.POST['second_number'])
+            is_possible_same_number = request.POST.get('is_possible_same_number', False)
             numbers_of_results = []
+            i = 0
 
-            for i in range(quantity_random):
-                numbers_of_results.append(randint(first_number, second_number))
-                print(numbers_of_results)
+            if first_number <= second_number:
+                pass
+            elif (second_number - first_number) > quantity_random:
+                pass
+
+            if is_possible_same_number:
+                for i in range(quantity_random):
+                    numbers_of_results.append(randint(first_number, second_number))
+                return render(request, 'random_numbers.html', {'form': form, 'random_done': True, 'form_result': numbers_of_results})
+            else:
+                while i != quantity_random:
+                    number = randint(first_number, second_number)
+                    if number in numbers_of_results:
+                        quantity_random = quantity_random + 1
+                    else:
+                        numbers_of_results.append(number)
+                    i += 1
 
             return render(request, 'random_numbers.html', {'form': form, 'random_done': True, 'form_result': numbers_of_results})
 
