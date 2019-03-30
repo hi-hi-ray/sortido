7d5c86e2d4b8562ccaba49261b44f08fd0dff6ab
hi-hi-ray <dutra.raysa@gmail.com>
2019-03-30 16:06:46 -0300

    :construction: deploy configuration

--- app/wsgi.py
+++ app/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+path = '/home/hihiray/sortido'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
